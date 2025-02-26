e978cda4789181d35672d32a905393b48617c46b
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-26 15:39:32 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,12 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+# .env फाइल लोड करें
 load_dotenv()
 
-DATABASE_URL = os.getenv("postgresql://exam_db_848p_user:4KtEKvxuF6R4LkQZAiUimku8EkgBLqZs@dpg-cutfgqbtq21c73bemns0-a.oregon-postgres.render.com/exam_db_848p")
+# वैरिएबल एक्सेस करें
+CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(CLOUDINARY_URL=cloudinary://684851764112618:BAYsU8wkFPUKEOB1-6xNKZdo2N8@dsuolsjlo)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-metadata = MetaData()
+print("Cloudinary URL:", CLOUDINARY_URL)
+print("Database URL:", DATABASE_URL)

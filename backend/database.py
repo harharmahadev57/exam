1e209e26eb868092b2eacaf244f7a3ef2e42aa46
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-28 12:10:05 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 
 # .env फाइल लोड करें
-load_dotenv()
+load_dotenv(dotenv_path=".env")  # <- यहाँ .env का पथ दें
 
 # वैरिएबल एक्सेस करें
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# चेक करें कि वैल्यू आ रही है या नहीं
 print("Cloudinary URL:", CLOUDINARY_URL)
 print("Database URL:", DATABASE_URL)
